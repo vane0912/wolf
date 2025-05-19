b8c7bf6f054a93461e387f47f8f1be7a81265fdb
Vanessa Gonzalez <vanessagpro5@gmail.com>
2025-05-19 10:47:46 -0600

    add password test

--- tests/Password.spec.js
+++ tests/Password.spec.js
@@ -25,4 +25,47 @@ test('Password_test', async ({ page }) => {
     await expect(login_cta).toBeEnabled()
     await login_cta.click()
     await page.waitForNavigation({waitUntil: 'load'})
-  })
+})
+
+test('Magic login link', async ({ page,browser }) => {
+    await page.goto(deploy_url + 'account/settings')
+    const user = page.locator('id=loggedInUserContainer')
+    await expect(user).toBeVisible()
+    await user.click()
+    const btn_logout = page.locator('id=btnLogout')
+    await expect(btn_logout).toBeVisible()
+    await btn_logout.click()
+    await page.goto(deploy_url + 'login')
+    await page.waitForURL('**/login')
+    const email_input = page.locator('id=email_login_input')
+    await expect(email_input).toBeVisible()
+    await email_input.fill(email_test)
+    const continue_login = page.locator('id=continue_button')
+    await expect(continue_login).toBeEnabled()
+    await continue_login.click()
+    
+    await page.locator("id=magic_login_link").click()
+    await page.waitForTimeout(2000)
+    await expect(page.getByText("Check your email, we sent a password free login link")).toBeVisible()
+
+    const context = await browser.newContext({
+        httpCredentials: {
+          username: 'admin',
+          password: 'testivisa5!',
+        },
+    });
+    const email = await context.newPage();
+    await email.goto(deploy_url + 'mail')
+    await email.getByText("Here's your automatic login link").first().click()
+    await expect(email.locator(".messageHeaders")).toBeVisible()
+    await expect(email.locator(".messageHeaders")).toContainText(email_test)
+    const iframe = email.frameLocator('iframe');
+    const [newTab] = await Promise.all([
+      email.context().waitForEvent('page'),
+      iframe.getByText('Log in').click(),
+    ]);
+
+    await newTab.waitForLoadState()
+    const new_tab_user = newTab.locator('id=loggedInUserContainer')
+    await expect(new_tab_user).toBeVisible()
+})
--- tests/urls.js
+++ tests/urls.js
@@ -1,4 +1,4 @@
-const deploy_url = 'https://deploy-20250512--558fd6ba.ivisatravel.visachinaonline.com/'
-const email_test = 'automations1@wolf.com'
+const deploy_url = 'https://deploy-20250519--30eaf798.ivisatravel.visachinaonline.com/'
+const email_test = 'automations@wolf.com'
 let Orders = ['84','83','82'] 
 module.exports = {deploy_url,  email_test, Orders}
