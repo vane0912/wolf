77bc1e4197654b2e41ddefbb86e4d55373cb7e31
vane0912 <117692080+vane0912@users.noreply.github.com>
2025-11-25 15:57:44 +0000

    changes automations

--- tests/urls.js
+++ tests/urls.js
@@ -1,3 +1,3 @@
 const deploy_url = 'https://deploy-20251125--f372a5a2.ivisatravel.visachinaonline.com/'
-const email_test = 'automations25@wolf.com'
+const email_test = 'automations24@wolf.com'
 module.exports = {deploy_url,  email_test}
