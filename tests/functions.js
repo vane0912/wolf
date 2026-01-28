f019ab60c1edf27472a730ee7722e60057ccb4a0
vane0912 <117692080+vane0912@users.noreply.github.com>
2026-01-28 20:01:03 +0000

    update payments spreedly

--- tests/functions.js
+++ tests/functions.js
@@ -1,28 +1,20 @@
 async function newPaymentCheckout(page,url,creditCard, cvvNum){
-    await page.waitForURL(url + 'step=review')
+  await page.waitForURL(url + 'step=review')
+    if(continuebtn){
+        await page.getByRole('button', { name: 'Continue to payment' }).click()
+    }
     await page.waitForTimeout(2000)
     const duplicate = await page.isVisible('id=btnDisclaimerNext')
     if (duplicate){
       await page.locator('id=btnDisclaimerNext').click()
     }
-    const stripeFrame = page.frameLocator('iframe[name^="__privateStripeFrame"]').nth(1)
-    
-    await stripeFrame.locator("id=payment-numberInput").fill(creditCard);
+    await page.getByPlaceholder("Card number").fill(creditCard);
 
-    const expiration_month = stripeFrame.locator("id=payment-expiryInput")
+    const expiration_month = page.getByPlaceholder("MM/YY")
     await expiration_month.fill('10/26')
 
-    const cvv = stripeFrame.locator("id=payment-cvcInput")
+    const cvv = page.getByPlaceholder("CVV")
     await cvv.fill(cvvNum)
-    const zip_code = stripeFrame.locator("id=payment-postalCodeInput")
-    await zip_code.fill('12345')
-    /*
-    const cardholder_name = page.getByPlaceholder("Cardholder name")
-    await cardholder_name.fill('John Smith')
-    
-    const zip_code = page.getByPlaceholder("ZIP code")
-    await zip_code.fill('12345')
-    */ 
 }
 
 module.exports = {newPaymentCheckout}
