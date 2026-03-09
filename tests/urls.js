c938d63f746ed4c5f0f3060d502ff845fa0f9f2b
vane0912 <vanessagpro5@gmail.com>
2026-03-09 17:54:15 -0600

    Save local changes before pulling

--- tests/urls.js
+++ tests/urls.js
@@ -1,3 +1,3 @@
-const deploy_url = 'https://deploy-20251215--7d49cb97.ivisatravel.visachinaonline.com/'
+const deploy_url = 'https://deploy-20260310--f6ff4c52.ivisatravel.visachinaonline.com/'
 const email_test = 'automations24@wolf.com'
 module.exports = {deploy_url,  email_test}
