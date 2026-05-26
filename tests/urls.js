9145d847899d2f62896f65a0a5987ff4e27c3102
vane0912 <vanessagpro5@gmail.com>
2026-05-26 11:16:41 -0600

    https://deploy-20260527--dcabe411.ivisatravel.visachinaonline.com/

--- tests/urls.js
+++ tests/urls.js
@@ -1,3 +1,3 @@
-const deploy_url = 'https://deploy-20260504--4ccb243a.ivisatravel.visachinaonline.com/'
+const deploy_url = 'https://deploy-20260527--dcabe411.ivisatravel.visachinaonline.com/'
 const email_test = 'automations23423@wolf.com'
 module.exports = {deploy_url,  email_test}
